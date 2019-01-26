462c04a066db440f1a1b30e263147b2b0e0b0b9a
Mihály Köles <mihaly.koles@prezi.com>
2019-01-26 22:06:32 +0100

    foto2.0 init

---
+++ foto2.0/precache-manifest.43425e17df8a5fbf2dc0cf8c485cd95d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46dc5b83001d06c9203c",
+    "url": "/foto2.0/static/css/main.4ec7090f.chunk.css"
+  },
+  {
+    "revision": "46dc5b83001d06c9203c",
+    "url": "/foto2.0/static/js/main.46dc5b83.chunk.js"
+  },
+  {
+    "revision": "eeb1cd67b28837a3ba76",
+    "url": "/foto2.0/static/css/1.81407e21.chunk.css"
+  },
+  {
+    "revision": "eeb1cd67b28837a3ba76",
+    "url": "/foto2.0/static/js/1.eeb1cd67.chunk.js"
+  },
+  {
+    "revision": "9e168c265b6f835f5e3b",
+    "url": "/foto2.0/static/js/runtime~main.9e168c26.js"
+  },
+  {
+    "revision": "4e7a09e42682806bd613108d1b716451",
+    "url": "/foto2.0/index.html"
+  }
+];
