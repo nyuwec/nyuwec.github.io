fd1a16f0e32368a095b6dca6e0b13e0289b8b2d9
Mihály Köles <mihaly.koles@prezi.com>
2019-02-12 17:33:54 +0100

    orientation change handling 2

---
+++ foto2.0/precache-manifest.c55a54e953a43f507deeb39f65dea391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a75e95d8055889d25b5",
+    "url": "/foto2.0/static/css/main.39562c1c.chunk.css"
+  },
+  {
+    "revision": "8a75e95d8055889d25b5",
+    "url": "/foto2.0/static/js/main.8a75e95d.chunk.js"
+  },
+  {
+    "revision": "bd0310a38217142f8ce9",
+    "url": "/foto2.0/static/css/1.a46eba70.chunk.css"
+  },
+  {
+    "revision": "bd0310a38217142f8ce9",
+    "url": "/foto2.0/static/js/1.bd0310a3.chunk.js"
+  },
+  {
+    "revision": "9e168c265b6f835f5e3b",
+    "url": "/foto2.0/static/js/runtime~main.9e168c26.js"
+  },
+  {
+    "revision": "98218927878c627f535a328b0f11fce1",
+    "url": "/foto2.0/index.html"
+  }
+];
